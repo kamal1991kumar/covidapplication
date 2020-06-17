d9fccdfb9d89d38034c181233f570bf42d1fd0a2
kamal kumar <kamal1991kumar@gmail.com>
2020-06-17 16:07:52 +0530

    code setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+
+export default function IndexPage() {
+
+  return (
+    <Layout>
+       <Card>
+       hwllo
+    </Card>
+    </Layout>
+  )
+}
